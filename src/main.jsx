63708a1b25ed2c1de1a0f7a0ef9348184de0dc9c
Ga-Mikaël <tchiki.tshiki@gmail.com>
2023-03-27 09:37:57 +0200

    Fixed the theme and the vite config

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import '@fontsource/julius-sans-one';
 import '@fontsource/alice';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from './CustomTheme/theme.jsx';
+import theme from './theme.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
