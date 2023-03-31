db35f43684fdc3996e8c36735794086ffc5f5a5c
Chloebert <berthier.chloe@wanadoo.fr>
2023-03-31 16:08:06 +0200

    string between {''}

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -6,7 +6,7 @@ const Introduction = () => {
     <Container maxW={{ sm: '90%', lg: '70%' }}>
       <Center>
         <Heading as='h1' align='center' mt={5} color='light.200' textShadow='1px 3px #e4686f'>
-          Welcome to the Mixer's Lounge
+          {`Welcome to the Mixer's Lounge`}
         </Heading>
       </Center>
       <Container maxW='100%' bg='light.50' borderRadius='lg' backdropFilter='saturate(200%) blur(16px)' mt={5} boxShadow='sm' p={0}>
@@ -21,12 +21,12 @@ const Introduction = () => {
           />
           <Box maxW='65%' mt={{ base: '1.25rem', lg: '0' }} mb={{ base: '1.25rem', lg: '0' }} mr={5} ml={5}>
             <Text color='secondary.700'>
-              The aim of this website is quite simple: making looking for a cocktail easy and fun. You can look through our listing, search
-              by name, ingredients or categories, but you can also take a little test and find out what your new favourite cocktail will be!
-              We hope you will enjoy your stay!
+              {
+                'The aim of this website is quite simple: making looking for a cocktail easy and fun. You can look through our listing, search by name, ingredients or categories, but you can also take a little test and find out what your new favourite cocktail will be! We hope you will enjoy your stay!'
+              }
             </Text>
             <Text fontSize='xs' color='secondary.600'>
-              Drinking alcohol is dangerous for your health. Remember to always drink responsibly.
+              {'Drinking alcohol is dangerous for your health. Remember to always drink responsibly.'}
             </Text>
           </Box>
         </Flex>
