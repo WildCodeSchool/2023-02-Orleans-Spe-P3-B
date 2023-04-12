5164f5003d4c11117bd5a0ed581ace0634c32b9b
Violeta Doncheva <violeta.doncheva90@gmail.com>
2023-04-12 12:09:41 +0200

    updates done on cocktail detail

--- src/components/DescriptionCocktail.jsx
+++ src/components/DescriptionCocktail.jsx
@@ -7,7 +7,6 @@ const DescriptionCocktail = () => {
   const params = useParams();
   let { drinkId } = useParams();
   useEffect(() => {
-    console.log('***', drinkId);
     if (drinkId) {
       fetch(`https:\\www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
         .then(response => response.json())
@@ -22,34 +21,62 @@ const DescriptionCocktail = () => {
         <WrapItem p={'5'} boxShadow='lg' w='500px' h='auto' borderRadius='lg' bg={'light.50'} backdropFilter='saturate(150%) blur(10px)'>
           <Box justify='center' as='b' p={'4'} position='relative'>
             <Stack divider={<StackDivider />}>
-              <Heading fontSize={'4xl'} mt={'5'} mb={'5'} color={'secondary.600'}>
+              <Heading fontSize={'40px'} mt={'5'} mb={'4'} color={'light.400'}>
                 {cocktail.strDrink}
               </Heading>
               <Spacer />
             </Stack>
-            {/* {cocktail.map(cocktail => (
-              <UnorderedList mt={'2'} color={'secondary.600'} key={index}>
-                <ListItem as='b' fontSize='xl'>
-                  {cocktail}
-                </ListItem>
+            <Spacer />
+            <Text fontSize='lg' color={'secondary.600'} mt={'6'} mb={'4'}>
+              <UnorderedList>
+                <ListItem>{cocktail.strAlcoholic}</ListItem>
+                <ListItem>{cocktail.strCategory}</ListItem>
+                <ListItem>{cocktail.strGlass}</ListItem>
               </UnorderedList>
-            ))}*/}
-            <Spacer />
-            <Text fontSize='2xl' color={'secondary.600'} mt={'6'} mb={'5'}>
-              {'Ingredients :'}
-              {cocktail.strIngredient1}
+            </Text>
+            <Text fontSize='xl' color={'light.400'} mt={'6'} mb={'3'}>
+              {' '}
+              {'Ingredients :'}{' '}
+            </Text>
+            <Text fontSize='2xl' color={'secondary.600'}>
+              <p>
+                {cocktail.strIngredient1} {cocktail.strMeasure1}
+              </p>
+              <p>
+                {cocktail.strIngredient2} {cocktail.strMeasure2}
+              </p>
+              <p>
+                {cocktail.strIngredient3} {cocktail.strMeasure3}
+              </p>
+              <p>
+                {cocktail.strIngredient4} {cocktail.strMeasure4}
+              </p>
+              <p>
+                {cocktail.strIngredient5} {cocktail.strMeasure5}
+              </p>
+              <p>
+                {cocktail.strIngredient6} {cocktail.strMeasure6}
+              </p>
             </Text>
             <Spacer />
-
-            <Text fontSize='2xl' color={'secondary.600'} mt={'6'}>
-              Instructions :
+            <Text fontSize='xl' color={'light.400'} mt={'6'}>
+              {'Instructions :'}
+            </Text>
+            <Text fontSize='2xl' color={'secondary.600'} mt={'3'}>
+              {cocktail.strInstructions}
             </Text>
             <Spacer />
-            <Text fontSize='xl' color={'secondary.600'} mt={'5'}></Text>
           </Box>
         </WrapItem>
         <WrapItem>
-          <Image width={{ base: '30rem' }} height={{ base: '50rem' }} objectFit='cover' />
+          <Image
+            src={cocktail.strDrinkThumb}
+            alt={cocktail.strDrink}
+            borderRadius='lg'
+            width={{ base: '30rem' }}
+            height={{ base: '50rem' }}
+            objectFit='cover'
+          />
         </WrapItem>
       </Wrap>
     </>
