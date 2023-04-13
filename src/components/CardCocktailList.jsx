3f61b18b7d762519c695eba791b990eb9a0a5b0a
Ga-Mikaël <tchiki.tshiki@gmail.com>
2023-04-13 10:15:57 +0200

    fix card search

--- src/components/CardCocktailList.jsx
+++ src/components/CardCocktailList.jsx
@@ -1,58 +1,24 @@
-import { Button, Card, CardBody, Center, Collapse, Container, Heading, HStack, Stack, Tag, TagLabel, Image, Box } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Button, Card, CardBody, Center, Heading, Stack, Image, Box } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
-const CardCocktailList = ({ strDrink, strDrinkThumb }) => {
-  const [isShown, setIsShown] = useState(false);
+const CardCocktailList = ({ strDrink, strDrinkThumb, idDrink }) => {
   return (
-    <Box mb={5}>
-      <Card
-        role='group'
-        onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}
-        bg='light.50'
-        backdropFilter='saturate(200%)'
-        margin='0.5rem'
-      >
+    <Box mb={10}>
+      <Card bg='light.50' boxShadow='base' margin='0.5rem'>
         <CardBody maxWidth='16rem' padding='0'>
           <Image borderRadius='lg' src={strDrinkThumb} alt={strDrink} zIndex={1} filter='auto' brightness='95%' padding='8px' />
-          <Container
-            display={'flex'}
-            _groupHover={{ display: 'none' }}
-            width='94%'
-            height='25%'
-            paddingTop='13%'
-            bgGradient='linear(rgba(255, 114, 75, 0),rgba(1,1,1, 0.2),rgba(1,1,1, 0.6),rgba(1,1,1, 1))'
-            position='absolute'
-            bottom='3%'
-            left='3%'
-            justifyContent='center'
-          >
-            <Heading
-              as='h2'
-              fontSize='1.2rem'
-              maxWidth='100%'
-              noOfLines={1}
-              display={'flex'}
-              _groupHover={{ display: 'none' }}
-              color='light.200'
-            >
-              {strDrink}
-            </Heading>
-          </Container>
-          <Collapse transition={{ enter: { duration: 0.7 } }} animateOpacity in={isShown}>
-            <Stack padding='1rem' spacing='5'>
-              <Center>
-                <Heading as='h2' noOfLines={1} size='md' color='secondary.600'>
-                  {strDrink}
-                </Heading>
-              </Center>
-              <Center>
-                <Button size='sm' mt={2} width='75%' variant='cardButton'>
-                  {'Have a look'}
-                </Button>
-              </Center>
-            </Stack>
-          </Collapse>
+          <Stack padding='1rem' spacing='5'>
+            <Center>
+              <Heading as='h2' size='md' color='secondary.600'>
+                {strDrink}
+              </Heading>
+            </Center>
+            <Center>
+              <Button size='sm' mt={2} width='75%' variant='cardButton' id='cocktailDetail'>
+                <Link to={`/description/${idDrink}`}>{'Have a look'}</Link>
+              </Button>
+            </Center>
+          </Stack>
         </CardBody>
       </Card>
     </Box>
